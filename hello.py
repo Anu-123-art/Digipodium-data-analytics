28cc973f73acbdebc11665825e73a5e2404ed229
Anushka <anushkasrivastava5599@gmail.com>
2023-06-25 13:59:28 +0530

    turtle

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
 print('hello world😊')
+"""n= int(input("enter a number:"))
+m= int(input("enter a number:"))
+p=m+n
+print("enter the result:",p)"""
+"""num=int(input("enter a number:"))
+if(num%2)==0:
+    
+        print("number is even",num)
+    
+else:
+    
+        print("no is odd",num)"""
+        
---
+++ square.py
@@ -0,0 +1,14 @@
+from turtle import *
+bk(120)
+rt(90)
+bk(120)
+rt(90)
+bk(120)
+rt(90)
+bk(120)
+rt(90)
+
+
+hideturtle()
+
+mainloop()
