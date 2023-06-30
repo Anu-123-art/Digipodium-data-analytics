4cd2caae25fba392ca52718b6c5cb59ffc565029
Anushka <anushkasrivastava5599@gmail.com>
2023-06-30 14:04:23 +0530

    my next programs loop

--- a.py
+++ a.py
@@ -1,2 +1,10 @@
 from turtle import*
 rt(60)
+bk(120)
+rt(60)
+fd(120)
+
+
+
+
+mainloop()
---
+++ hex.py
@@ -0,0 +1,13 @@
+from turtle import*
+speed('fastest')
+pencolor('blue')
+pensize(2)
+for i in range(100):
+     fd(100-i)
+     rt(60)
+     circle(100,280)
+     dot(10,'green')
+
+
+
+mainloop()
